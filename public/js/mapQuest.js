3a612b2088c43fa89fa795d57c6a946477512dfb
Aung Yang <aungkyang622@gmail.com>
2021-02-25 13:59:18 -0800

    changes

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
-	// add your code here
+	
+
+	L.mapquest.key = {$APIKEY};
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117,236],
+		layers: L.mapquest.titleLayer('map)',
+		zoom: 12,
+		zoomControl: false
+	});
+
+
+	L.marker([32.88, -117.236]).addTo(map);
+
 }
